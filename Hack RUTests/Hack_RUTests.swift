b6101d3ddac4a8a0777a560ce284ea33fbb18592
Sai Nimish Bazar <bazarsainimish29@gmail.com>
2025-10-04 12:14:50 -0400

    Reorganized folder structure

---
+++ Hack RUTests/Hack_RUTests.swift
@@ -0,0 +1,16 @@
+//
+//  Hack_RUTests.swift
+//  Hack RUTests
+//
+//  Created by Sai Nimish Bazar on 10/4/25.
+//
+
+import Testing
+
+struct Hack_RUTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
